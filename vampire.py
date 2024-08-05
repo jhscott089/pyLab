7ff1068a21c473f3a29639d149209f42d3a7467e
jhscott89 <jamesscott6@gmail.com>
2024-08-04 20:07:01 -0600

    Add various practice lessons

--- vampire.py
+++ vampire.py
@@ -1,12 +1,5 @@
-# vampire.py
-
-name = 'Carol'
-age = 3000
-if name == 'Alice':
-    print('Hi, Alice.')
-elif age < 12:
-    print('You are not Alice, kiddo.')
-elif age > 2000:
-    print('Unlike you, Alice is not an undead, immortal vampire.')
-elif age > 100:
-    print('You are not Alice, grannie.')
+name = ''
+while name != 'your name':
+    print('Please type your name.')
+    name = input()
+print('Thank you!')
