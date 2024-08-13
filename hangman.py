538451f71e03b9a2833682fa375b4b9e96fd1259
jhscott89 <jamesscott6@gmail.com>
2024-08-12 18:50:22 -0600

    Created function to cause failure if player makes x incorrect guesses

--- hangman.py
+++ hangman.py
@@ -1,6 +1,19 @@
 word = 'wordle'
 length = (len(word))
-print('One word, ' + str(length) + ' letters')
-print('Guess a letter')
-letterList = word[0:length]
-print(letterList)
+remaining = 6
+while remaining > 0:
+    print('One word, ' + str(length) + ' letters')
+    print('Guess a letter')
+    letterList = word[0:length]
+    guess = input()
+    if guess in letterList:
+        result = 1
+    else:
+        result = 0
+    print(result)
+    if result == 1:
+        print('Congratulations! You guessed correctly. Your score is now ' + str(result))
+    else:
+        remaining = remaining - 1
+        print('WRONG. You have ' + str(remaining) + ' tries remaining')
+
