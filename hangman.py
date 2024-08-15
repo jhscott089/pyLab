66049ee9b67038a0cd5b665142f43addfa2fdfff
jhscott89 <jamesscott6@gmail.com>
2024-08-14 21:25:22 -0600

    Put game in a function

--- hangman.py
+++ hangman.py
@@ -1,39 +1,42 @@
 word = 'wordle'
-length = (len(word))
-remaining = int(length)
-import sys
-cGuessCount = 0
-guessList = []
-while remaining > 0:
-    print('One word, ' + str(length) + ' letters')
-    print('Guess a letter')
-    pl = str(guessList)
-    if pl != '[]':
-        print('Guesses so far: ' + pl)
-    letterList = word[0:length]
-    guess = input()
-    if len(guess) > 1:
-        print('Input invalid: May only guess single letter.')
-        break
-    guessList += guess
 
-    if guess in letterList:
-        result = 1
-    else:
-        result = 0
-    if result == 1:
-        cGuessCount = cGuessCount + 1
-        if cGuessCount == length:
-            print('Congratulations, you won! Play again? Type "Y" or "N"')
-            input()
-        else:
-            print('Congratulations! You guessed correctly. Your score is now ' + str(cGuessCount))
-    else:
-        remaining = remaining - 1
-        if remaining == 0:
-            print('You have died.')
+def game():
+    length = (len(word))
+    remaining = int(length)
+    import sys
+    cGuessCount = 0
+    guessList = []
+    while remaining > 0:
+        print('One word, ' + str(length) + ' letters')
+        print('Guess a letter')
+        pl = str(guessList)
+        if pl != '[]':
+            print('Guesses so far: ' + pl)
+        letterList = word[0:length]
+        guess = input()
+        if len(guess) > 1:
+            print('Input invalid: May only guess single letter.')
             break
+        guessList += guess
+
+        if guess in letterList:
+            result = 1
         else:
-            print('WRONG. You have ' + str(remaining) + ' tries remaining.')
+            result = 0
+        if result == 1:
+            cGuessCount = cGuessCount + 1
+            if cGuessCount == length:
+                print('Congratulations, you won! Play again? Type "Y" or "N"')
+                input()
+            else:
+                print('Congratulations! You guessed correctly. Your score is now ' + str(cGuessCount))
+        else:
+            remaining = remaining - 1
+            if remaining == 0:
+                print('You have died.')
+                break
+            else:
+                print('WRONG. You have ' + str(remaining) + ' tries remaining.')
 
 
+game()
