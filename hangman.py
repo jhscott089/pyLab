57f04f373543ed860a8dcdb39d4b0a3b287ffa0f
jhscott89 <jamesscott6@gmail.com>
2024-08-14 21:16:27 -0600

    Created list to report correct guesses

--- hangman.py
+++ hangman.py
@@ -2,26 +2,31 @@ word = 'wordle'
 length = (len(word))
 remaining = int(length)
 import sys
-guessList = 0
+cGuessCount = 0
+guessList = []
 while remaining > 0:
     print('One word, ' + str(length) + ' letters')
     print('Guess a letter')
+    pl = str(guessList)
+    if pl != '[]':
+        print('Guesses so far: ' + pl)
     letterList = word[0:length]
     guess = input()
+    guessList += guess
 
     if guess in letterList:
         result = 1
     else:
         result = 0
     if result == 1:
-        guessList = guessList + 1
-        print('Congratulations! You guessed correctly. Your score is now ' + str(guessList))
+        cGuessCount = cGuessCount + 1
+        print('Congratulations! You guessed correctly. Your score is now ' + str(cGuessCount))
     else:
         remaining = remaining - 1
         if remaining == 0:
             print('You have died.')
             break
         else:
-            print('WRONG. You have ' + str(remaining) + ' tries remaining. Letters guessed so far:' + str(guessList))
+            print('WRONG. You have ' + str(remaining) + ' tries remaining.')
 
 
