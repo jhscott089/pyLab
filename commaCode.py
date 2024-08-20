97198b21c256d9db6476ce0ed15dd2fcc5501a81
jhscott89 <jamesscott6@gmail.com>
2024-08-19 20:05:55 -0600

    added randomCoin.py

--- commaCode.py
+++ commaCode.py
@@ -1,12 +1,12 @@
+# A function that takes a list value as an argument and returns a string with all the items separated by a comma and a space, with and inserted before the last item.
+
 def commaCode(list1):
     lLength= len(list1)
     lString = ''
     for i in list1[0:lLength - 1]:
         lString += i + ', '
-    lString += "and " + list1[lLength - 1] + "."
-    print(lString)
+    lString += "and " + list1[-1] + "."
+    return(lString)
 
-
-
-spam = []
-commaCode(spam)
+spam = ["a","b"]
+print(commaCode(spam))
---
+++ randomCoin.py
@@ -0,0 +1,9 @@
+#Program to find out how often a streak of six heads or six tails comes up in a randomly generated list of heads and tails.
+#
+# import random
+numberOfStreaks = 0
+for experimentNumber in range(10000):
+    # Code that creates a list of 100 'heads' or 'tails' values.
+
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
